1c2f9efe114efadafb7b5e1a85fe3d5924e4d59c
hashishy <77440642+Hashish-y@users.noreply.github.com>
2021-07-07 22:11:51 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/5_Beautiful_Year.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int y;
+    cin >> y;
+    while (y < 1000 && y>9000);
+	for (int i = y + 1;; i++){
+		int a = i % 10;
+		int b = (i / 10) % 10;
+		int c = (i / 100) % 10;
+		int d = (i / 1000) % 10;
+		if (a != b && b != c && c != d && a != c && b != d && a != d){
+			cout << i << endl;
+			break;
+		}
+	}
+}
+
+
