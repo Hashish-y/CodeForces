a9a998c206e1759f86f74a07637b382439b78833
hashishy <77440642+Hashish-y@users.noreply.github.com>
2021-07-08 20:14:58 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/7_Word.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string change_to_up(string s){
+    for(int i=0; i<s.length(); i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            s[i]-=32;
+        }
+    }
+    return s;
+}
+
+string change_to_down(string s){
+    for(int i=0; i<s.length(); i++){
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            s[i]+=32;
+        }
+    }
+    return s;
+}
+
+int main(){
+    string s;
+    cin >> s;
+
+    int l=0, u=0;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            l++;
+        }
+        if(s[i] >= 'A' && s[i] <= 'Z'){
+            u++;
+        }
+    }
+
+
+    if(l<u) {
+        cout << change_to_up(s) << endl;
+    }
+    else{
+        cout << change_to_down(s) << endl;
+    }
+
+    return 0;
+}
