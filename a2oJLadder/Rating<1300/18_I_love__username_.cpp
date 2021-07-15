56a22cb29cd8a783339ecc70b9942a746b940f18
hashishy <77440642+Hashish-y@users.noreply.github.com>
2021-07-15 21:51:38 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/18_I_love__username_.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int max = arr[0], min = arr[0], amazing_1=0, amazing_2 = 0;
+    for(int i=1; i<n; i++){
+        if(max <arr[i]){
+            amazing_1++;
+            max = arr[i];
+        }
+        if(min > arr[i]){
+            amazing_2++;
+            min = arr[i];
+        }
+    }
+
+    cout << amazing_1+amazing_2 << endl;
+
+    return 0;
+
+}
