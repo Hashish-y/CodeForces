bb2f30fb6b0dc5aad16152b9b1165614ff795c7d
hashishy <77440642+Hashish-y@users.noreply.github.com>
2021-07-15 21:32:29 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/16_Insomnia_cure.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+ 
+
+
+ 
+int main(){
+
+int k,l,m,n,d;
+
+cin>>k>>l>>m>>n>>d;
+
+int count=0;
+
+for(int i=1;i<=d;i++){
+
+    if((i%k)==0 || (i%l)==0 ||(i%m)==0 ||(i%n)==0 ){
+
+        count=count+1;
+}
+
+}
+
+
+cout<<count;
+return 0;
+
+}
