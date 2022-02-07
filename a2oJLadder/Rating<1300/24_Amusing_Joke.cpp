3139b371c7af817702d6016c78571c5702d64e70
hashishy <77440642+Hashish-y@users.noreply.github.com>
2022-02-07 12:30:15 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/24_Amusing_Joke.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string x,y,z;
+    cin >> x >> y >> z;
+    x = x+y;
+    sort(x.begin(), x.end());
+    sort(z.begin(), z.end());
+
+    if(x == z){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
