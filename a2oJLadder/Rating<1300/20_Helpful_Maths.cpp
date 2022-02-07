11c863c86a37ebcfc7f2437ae2acb96ee03ad40f
hashishy <77440642+Hashish-y@users.noreply.github.com>
2022-02-07 11:44:06 +0530

    Add files via upload

---
+++ a2oJLadder/Rating<1300/20_Helpful_Maths.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    int count = 0, arr[1000];
+    cin >> s;
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '+'){
+            continue ;
+        }
+        else{
+            arr[count++] = s[i] - '0';
+        }
+    }
+
+    sort(arr, arr+count);
+
+    for(int i=0; i<count; i++){
+        cout << arr[i] ;
+        if(i== count-1){
+            break;
+        }
+        cout << "+";
+    }
+
+    return 0;
+}
